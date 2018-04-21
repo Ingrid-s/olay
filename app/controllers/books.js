303fb5f9f7e0f0bf0f7c483c68a4f1d92dd797da
IvonReD <ivonne.reyes.dc@gmail.com>
2018-04-20 21:20:24 -0500

    Subiendo proyecto

---
+++ app/controllers/books.js
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller';
+
+import Ember from 'ember';  
+export default Ember.Controller.extend({  
+  actions: {
+    deleteBook(book) {
+      book.deleteRecord();
+      book.save();
+    },
+  },
+});
---
+++ app/controllers/books/new.js
@@ -0,0 +1,17 @@
+// import Controller from '@ember/controller';
+
+
+import Ember from 'ember';  
+export default Ember.Controller.extend({  
+  actions: {
+    publishBook: function() {
+      var newBook = this.store.createRecord('book', {
+        title: this.get('title'),
+        author: this.get('author'),
+        pubYear: this.get('pubYear'),
+      });
+      newBook.save();
+      this.transitionTo('books');
+    },
+  },
+});
